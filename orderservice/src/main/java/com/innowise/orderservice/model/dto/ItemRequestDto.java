1b2c37176233524af4d147cbf96a3119c4850c49
SLSRK <ivans2001slesarenko@gmail.com>
2026-08-12 17:07:26 +0300

    Fix: typo

--- orderservice/src/main/java/com/innowise/orderservice/model/dto/ItemRequestDto.java
+++ orderservice/src/main/java/com/innowise/orderservice/model/dto/ItemRequestDto.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Size;
 public record ItemRequestDto(
 
         @NotBlank(message = "Name cannot be empty")
-        @Size(max = 100, message = "Item hame must be no longer than 100 characters")
+        @Size(max = 100, message = "Item name must be no longer than 100 characters")
         String name,
 
         @NotNull(message = "Price cannot be null")
